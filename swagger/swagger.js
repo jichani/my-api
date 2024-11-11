14e2b25444ed4445d120f29194b9ef4b78669cda
Jichani <123962013+jichani@users.noreply.github.com>
2024-11-11 09:57:33 +0900

    에러 수정

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -9,36 +9,40 @@ const swaggerOptions = {
       version: "1.0.0",
       description: "API documentation for managing animals",
     },
-  },
-  apis: ["./routes/*.js"],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-module.exports = swaggerDocs;
-
-swaggerOptions.swaggerDefinition.components = {
-  schemas: {
-    Animal: {
-      type: "object",
-      properties: {
-        id: {
-          type: "integer",
-          description: "동물 ID",
-        },
-        species: {
-          type: "string",
-          description: "동물 종류",
-        },
-        name: {
-          type: "string",
-          description: "동물 이름",
-        },
-        birthDate: {
-          type: "string",
-          format: "date",
-          description: "동물 태어난 날짜",
+    components: {
+      schemas: {
+        Animal: {
+          type: "object",
+          properties: {
+            id: {
+              type: "integer",
+              description: "동물 ID",
+              example: 1,
+            },
+            species: {
+              type: "string",
+              description: "동물 종류",
+              example: "Dog",
+            },
+            name: {
+              type: "string",
+              description: "동물 이름",
+              example: "Buddy",
+            },
+            birthDate: {
+              type: "string",
+              format: "date",
+              description: "동물 태어난 날짜",
+              example: "2020-05-15",
+            },
+          },
+          required: ["species", "name", "birthDate"],
         },
       },
     },
   },
+  apis: ["./routes/*.js"], // API 주석이 포함된 파일 경로
 };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = swaggerDocs;
